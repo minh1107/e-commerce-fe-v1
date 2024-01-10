3aafc7da769057d2834c212f6d65debe4d41adbf
minh1107 <minhdepzaiahihi@gmail.com>
2024-01-10 13:49:31 +0700

    fix bug deploy

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {persistor, store} from "./stores/redux";
 import { Provider } from "react-redux";
-import {  RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider >
+        <BrowserRouter >
           <App />
-        </RouterProvider>
+        </BrowserRouter>
       </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 reportWebVitals();
